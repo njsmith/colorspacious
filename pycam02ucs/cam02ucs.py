58c7c0625a86cd018cb7ebfc8226f4100887b1ab
Futrell <futrell@mit.edu>
2014-07-10 13:43:15 -0400

    remove J'a'b'

--- pycam02ucs/cam02ucs.py
+++ pycam02ucs/cam02ucs.py
@@ -32,48 +32,51 @@ def JMh_to_sRGB(J, M, h):
     return sRGB_to_XYZ(*JMh_to_XYZ(J, M, h))
 
 
-#############  JMh <=> J' a' b' #################
+#############  JMh <=> J/K a' b' #################
 # Equations (4) from Luo et al (2006)
+# J/K a' b' is a Euclidean space corresponding to Luo et al (2006)'s perceptual
+# distance metrics. Which metric depends on the values of KL, c1, and c2 used
+# for conversion. 
 
-def JMh_to_Jpapbp(J, M, h, c1, c2):
+def JMh_to_JKapbp(J, M, h, KL, c1, c2):
     Jp = (1 + 100 * c1) * J / (1 + c1 * J)
+    JK = Jp / K
     Mp = (1. / c2) * np.log(1 + c2 * M)
     h_rad = np.deg2rad(h)
     ap = Mp * np.cos(h_rad)
     bp = Mp * np.sin(h_rad)
-    return Jp, ap, bp
+    return JK, ap, bp
 
-def Jpapbp_to_JMh(Jp, ap, bp, c1, c2):
-    raise NotImplementedError
+def JKapbp_to_JMh(JK, ap, bp, KL, c1, c2):
+    Jp = JK * KL
+    J = Jp / (c1 * Jp - 100 * c1 - 1)
+    # ap = Mp * cos(h)
+    # bp = Mp * sin(h)
+    # Mp = bp/sin(h)
+    # ap = (bp/sin(h)) * cos(h)
+    # ap = bp * sin(h)/cos(h)
+    # cos(h) = bp/ap * sin(h)
+    # solve numerically for h in the interval of possible hue degrees
+    h = TODO
+    Mp = bp/sin(h)
+    assert np.allclose(Mp, ap/cos(h))
+    M = (np.exp(c2*Mp) - 1)/c2
+    return J, M, h
 
-
-###########  J' a' b' <=> J/K a' b' ##################
-# J/K a' b' is a Euclidean space corresponding to Luo et al (2006)'s perceptual
-# distance metrics. Which metric depends on the values of KL, c1, and c2 used
-# for conversion.
-
-def Jpapbp_to_JKapbp(Jp, ap, bp, KL):
-    return Jp/KL, ap, bp
-
-def JKapbp_to_Jpapbp(JK, ap, bp, KL):
-    return Jp*KL, ap, bp
-    
 
 ###########  sRGB <=> J/K a' b' ##################
 # Full pipeline from sRGB to the uniform space J/K a' b'.
-# sRGB -> XYZ -> JMh -> J' a' b' -> J/K a' b'
+# sRGB -> XYZ -> JMh -> J/K a' b'
 
 def sRGB_to_JKapbp(R, G, B, mode='UCS'):
     KL, c1, c2 = get_KL_c1_c2(mode)
     X, Y, Z = sRGB_to_XYZ(R, G, B)
     J, M, h = XYZ_to_JMh(X, Y, Z)
-    Jp, ap, bp = JMh_to_Jpapbp(J, M, h, c1, c2)
-    return Jpapbp_to_JKapbp(Jp, ap, bp, KL)
+    return JMh_to_JKapbp(J, M, h, KL, c1, c2)
 
 def JKapbp_to_sRGB(JK, ap, bp, mode='UCS'):
     KL, c1, c2 = get_KL_c1_c2(mode)
-    Jp, ap, bp = JKapbp_to_Jpapbp(JK, ap, bp, KL)
-    J, M, h = Jpapbp_to_JMh(Jp, ap, bp, c1, c2)
+    J, M, h = JKapbp_to_JMh(JK, ap, bp, KL, c1, c2)
     X, Y, Z = JMh_to_XYZ(J, M, h)
     return XYZ_to_sRGB(X, Y, Z)
 
@@ -83,11 +86,11 @@ def JKapbp_to_sRGB(JK, ap, bp, mode='UCS'):
 # to J/K a' b' and taking Euclidean distance.
     
 def deltaEp_JMh(J1, M1, h1, J2, M2, h2, KL, c1, c2):
-    Jp1, ap1, bp1 = JMh_to_Jpapbp(J1, M1, h1, c1, c2)
-    Jp2, ap2, bp2 = JMh_to_Jpapbp(J2, M2, h2, c1, c2)
+    JK1, ap1, bp1 = JMh_to_JKapbp(J1, M1, h1, KL, c1, c2)
+    JK2, ap2, bp2 = JMh_to_JKapbp(J2, M2, h2, KL, c1, c2)
 
     return np.sqrt(
-        ((Jp1 - Jp2) / KL) ** 2
+        (JK - JK2) ** 2
         + (ap1 - ap2) ** 2
         + (bp1 - bp2) ** 2
         )
@@ -116,33 +119,3 @@ def deltaEp_sRGB(R1, G1, B1, R2, G2, B2, mode="UCS"):
     J2, M2, h2 = sRGB_to_JMh(R2, G2, B2)
     return deltaEp_JMh(J1, M1, h1, J2, M2, h2, KL, c1, c2)
 
-def test_JKapbp():
-    R1, G1, B1 = [1], [1], [1] # white
-    R2, G2, B2 = [0], [0], [0] # black
-    distance1 = deltaEp_sRGB(R1, G1, B1, R2, G2, B2, mode='UCS')
-
-    JK1, ap1, bp1 = sRGB_to_JKapbp(R1, G1, B1, mode='UCS')
-    JK2, ap2, bp2 = sRGB_to_JKapbp(R2, G2, B2, mode='UCS')
-    distance2 = np.sqrt((JK1 - JK2)**2
-                        + (ap1-ap2)**2
-                        + (bp1-bp2)**2)
-
-    assert abs(distance1-distance2) < .001
-
-    distance1L = deltaEp_sRGB(R1, G1, B1, R2, G2, B2, mode='LCD')
-    JK1, ap1, bp1 = sRGB_to_JKapbp(R1, G1, B1, mode='LCD')
-    JK2, ap2, bp2 = sRGB_to_JKapbp(R2, G2, B2, mode='LCD')
-    distance2L = np.sqrt((JK1 - JK2)**2
-                         + (ap1-ap2)**2
-                         + (bp1-bp2)**2)
-    
-    assert abs(distance1L-distance2L) < .001
-
-    distance1S = deltaEp_sRGB(R1, G1, B1, R2, G2, B2, mode='SCD')
-    JK1, ap1, bp1 = sRGB_to_JKapbp(R1, G1, B1, mode='SCD')
-    JK2, ap2, bp2 = sRGB_to_JKapbp(R2, G2, B2, mode='SCD')
-    distance2S = np.sqrt((JK1 - JK2)**2
-                         + (ap1-ap2)**2
-                         + (bp1-bp2)**2)
-    
-    assert abs(distance1S-distance2S) < .001    
